392cef4a34e448615cdae66425191221286a992b
superxiu <113812870+superxiu@users.noreply.github.com>
2024-09-10 20:30:53 +0800

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -33,7 +33,7 @@ import { ElNotification } from 'element-plus';
 import { useRouter } from 'vue-router';  
 import axios from '@/axios';  // 引入 axios 实例
 import { useUserStore } from '@/stores/userStore';  // 导入 user store
-import NET from '../../public/js/vanta.net.min.js'; // 引入Vanta.NET
+import { NET } from 'vanta/net'; // 正确导入Vanta.NET效果
   
 const apiUrl = import.meta.env.VITE_API_URL;
 // 构建请求的完整URL
