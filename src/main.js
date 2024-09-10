f5513c725685d8605187896c246af98d5cd9a4d5
superxiu <113812870+superxiu@users.noreply.github.com>
2024-09-10 20:04:03 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ import i18n from './i18n';
 import './styles/element-variables.scss' // 引入自定义样式
 import 'magic.css/dist/magic.min.css'; // 引入 Magic.css
 import * as THREE from '../public/js/three.min.js';
-import VANTA from '../public/js/vanta.net.min.js';
+import { VANTA } from '../public/js/vanta.net.min.js';
 
 const pinia = createPinia()
 
